2c2f4e1be94c70d501a47cbd2a3de5d12e9d3dd3
ChrisIngoeTlc <christopher.ingoe@thomsonlocal.com>
2015-04-07 15:29:40 +0100

    add Debug npm module

--- callbacks.js
+++ callbacks.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var debug = require('debug')('app');
+
 //different approaches:
 //1. Nested
 //2. Use Async module
@@ -7,10 +9,12 @@
 //4. Use generators (Node v11)
 
 exports.AddTwoNumbers = function AddTwoNumbers(num1, num2, callback){
+    debug('AddTwoNumbers is running...');
     callback(null, num1 + num2);
 };
 
 exports.AddTwoNumbersTheHardWay = function(num1, num2, callback){
+    debug('AddTwoNumbersTheHardWay is running...');
     NumberAddition(num1,num2,function(err, result){
         NumberSubtract(result,num2,function(err,result){
             NumberAddition(result, num2, function(err,result){
